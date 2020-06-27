a8ff03375153078ad63a2e0e795e13f2d8caac83
Michelle Chou <chou.m.rdh@gmail.com>
2020-06-26 20:27:10 -0700

    created workout-controller, completed html routes

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     exercises: {
+        date: {
+            type: Date,
+            default: Date.now()
+        },
+         type: {
+            type: String,
+            trim: true,
+            required: true
+        },
         name: {
             type: String,
             trim: true,
             required: "Name of Exercise is required"
-        },
-        type: {
-            type: String,
-            trim: true,
-            required: true
         },
         weight: Number,
         sets: Number,
@@ -23,4 +27,4 @@ const WorkoutSchema = new Schema({
     }
 })
 const Workout = mongoose.model("Workout", WorkoutSchema);
-module,exports = Workout;
+module, exports = Workout;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const workoutController = require("./controllers/workout-controller")
 const PORT = process.env.PORT || 8080;
 
 // const User = require("./userModel.js");
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(workoutController);
 
 mongoose.connect("mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
