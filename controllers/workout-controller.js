0d4381b11b0e536c214c81cd50a8269926b6ffdb
Michelle Chou <chou.m.rdh@gmail.com>
2020-06-30 20:10:36 -0700

    get total duration

--- controllers/workout-controller.js
+++ controllers/workout-controller.js
@@ -21,6 +21,41 @@ router.get("/api/workouts", async (req, res) => {
     try {
         let data = await db.Workout.find({})
 
+        //Function to take data array from db and perform summation of exercise lengths for each day.
+        const getDuration = function(data){
+
+            let newData = [];
+            //set new data array
+
+            //moves through each day of the array
+            data.forEach(function(day){
+                //initialize sum of duration (on each day)
+                let addedDuration = 0;
+
+                //move through each exercise performed during that day, and add the duration to the sum of durations.
+                day.exercises.forEach(function(exercise) {
+                    addedDuration = addedDuration + exercise.duration;
+                    return addedDuration;
+                });
+                //reconstruct day object including a totalDuration key value pairing.
+                // day.totalDuration = addedDuration;
+                const newDay = {
+                    "day": day.day,
+                    "_id": day._id,
+                    "exercises": day.exercises,
+                    "totalDuration": addedDuration
+                };
+                
+                //adds each day to the array of days (effectively building a new data array)
+                return newData.push(newDay);
+            })
+
+            //return the completely restructured data object with totalDuration added to each day.
+            return newData;
+        };
+
+        //call function and overwrite old data array
+        data = getDuration(data);
         res.json(data);
 
     } catch (error) {
