aa238a19afbea645444d26634fa201a387805c31
Michelle Chou <chou.m.rdh@gmail.com>
2020-08-21 18:37:17 -0700

    heroku atlas set up

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(workoutController);
-
+// mongodb+srv://dbUser:P@ssword@cluster0.q6l5y.mongodb.net/heroku_d9306550?retryWrites=true&w=majority
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutDB";
 mongoose.connect( MONGODB_URI, {
     useNewUrlParser: true,
